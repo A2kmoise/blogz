0254592668a011ecaa9957e53d30c21ba3528d10
A2kmoise <abayomoise950@gmail.com>
2026-05-27 09:37:52 +0200

    -{ * * }- Blog controller

---
+++ src/main/java/codewithmoise/org/blogbackend/controllers/BlogController.java
@@ -0,0 +1,11 @@
+package codewithmoise.org.blogbackend.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/blog")
+public class BlogController {
+
+}
