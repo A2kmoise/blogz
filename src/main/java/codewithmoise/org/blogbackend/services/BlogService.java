614baef2b12d65d148e5b0bd267ec45ba90f74ce
A2kmoise <abayomoise950@gmail.com>
2026-05-27 16:39:37 +0200

    -{ * * }- BlogService update

--- src/main/java/codewithmoise/org/blogbackend/services/BlogService.java
+++ src/main/java/codewithmoise/org/blogbackend/services/BlogService.java
@@ -2,41 +2,71 @@ package codewithmoise.org.blogbackend.services;
 
 import codewithmoise.org.blogbackend.DTO.requests.BlogPostRequest;
 import codewithmoise.org.blogbackend.DTO.requests.BlogUpdateRequest;
+import codewithmoise.org.blogbackend.DTO.responses.BlogResponse;
 import codewithmoise.org.blogbackend.models.Blog;
+import codewithmoise.org.blogbackend.models.User;
 import codewithmoise.org.blogbackend.repository.BlogRepository;
+import codewithmoise.org.blogbackend.repository.UserRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class BlogService {
 
     private final BlogRepository blogRepository;
+    private final UserRepository userRepository;
 
-    public BlogService(BlogRepository blogRepository){
+    public BlogService(BlogRepository blogRepository, UserRepository userRepository){
         this.blogRepository = blogRepository;
+        this.userRepository = userRepository;
     }
 
-    public Blog getBlogs(){
-       return (Blog) blogRepository.findAll();
+    public BlogResponse getBlogs(){
+       return (BlogResponse) blogRepository.findAll();
     }
     public Optional<Blog> getBlogById(Long id){
         return blogRepository.findById(id);
     }
-    public void createBlog(BlogPostRequest blogPostRequest){
+    public BlogResponse createBlog(BlogPostRequest blogPostRequest){
+
+        User user = userRepository.findById(blogPostRequest.getUserId())
+                .orElseThrow(() -> new RuntimeException("user not found"));
+
         Blog blog = new Blog();
 
         blog.setTitle(blogPostRequest.getTitle());
         blog.setContent(blogPostRequest.getContent());
         blog.setCategory(blogPostRequest.getCategory());
         blog.setTags(blogPostRequest.getTags());
+        blog.setUser(user);
+
+        Blog savedBlog = blogRepository.save(blog);
+
+        BlogResponse response = new BlogResponse();
+
+        response.setId(savedBlog.getId());
+        response.setTitle(savedBlog.getTitle());
+        response.setAuthorId(savedBlog.getUser().getId());
+        return response;
     }
-    public void updateBlog(BlogUpdateRequest blogUpdateRequest, Long blogId){
+    public BlogResponse updateBlog(BlogUpdateRequest blogUpdateRequest, Long blogId){
         Blog blog = blogRepository.findById(blogId)
                 .orElseThrow(()->  new RuntimeException("Blog not found"));
 
             blog.setContent(blogUpdateRequest.getContent());
             blog.setTags(blogUpdateRequest.getTags());
 
+            Blog updatedBlog = blogRepository.save(blog);
+
+                BlogResponse response = new BlogResponse();
+                response.setContent(updatedBlog.getContent());
+                response.setTags(updatedBlog.getTags());
+
+                return response;
+
     }
+
     public void deleteBlog(Long blogId){
         Blog blog = blogRepository.findById(blogId)
                 .orElseThrow(()-> new RuntimeException("Blog not found"));
