cfe47f166b2eff79a1d632047987b9a93364f065
A2kmoise <abayomoise950@gmail.com>
2026-05-27 15:49:55 +0200

    -{ * * }- blog service methods

--- src/main/java/codewithmoise/org/blogbackend/services/BlogService.java
+++ src/main/java/codewithmoise/org/blogbackend/services/BlogService.java
@@ -1,4 +1,46 @@
 package codewithmoise.org.blogbackend.services;
 
+import codewithmoise.org.blogbackend.DTO.requests.BlogPostRequest;
+import codewithmoise.org.blogbackend.DTO.requests.BlogUpdateRequest;
+import codewithmoise.org.blogbackend.models.Blog;
+import codewithmoise.org.blogbackend.repository.BlogRepository;
+
+import java.util.Optional;
+
 public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    public BlogService(BlogRepository blogRepository){
+        this.blogRepository = blogRepository;
+    }
+
+    public Blog getBlogs(){
+       return (Blog) blogRepository.findAll();
+    }
+    public Optional<Blog> getBlogById(Long id){
+        return blogRepository.findById(id);
+    }
+    public void createBlog(BlogPostRequest blogPostRequest){
+        Blog blog = new Blog();
+
+        blog.setTitle(blogPostRequest.getTitle());
+        blog.setContent(blogPostRequest.getContent());
+        blog.setCategory(blogPostRequest.getCategory());
+        blog.setTags(blogPostRequest.getTags());
+    }
+    public void updateBlog(BlogUpdateRequest blogUpdateRequest, Long blogId){
+        Blog blog = blogRepository.findById(blogId)
+                .orElseThrow(()->  new RuntimeException("Blog not found"));
+
+            blog.setContent(blogUpdateRequest.getContent());
+            blog.setTags(blogUpdateRequest.getTags());
+
+    }
+    public void deleteBlog(Long blogId){
+        Blog blog = blogRepository.findById(blogId)
+                .orElseThrow(()-> new RuntimeException("Blog not found"));
+
+         blogRepository.delete(blog);
+    }
 }
