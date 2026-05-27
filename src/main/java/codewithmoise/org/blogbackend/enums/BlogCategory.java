b351e8db44d949d762b1d761eb17c2d2cc4f7315
A2kmoise <abayomoise950@gmail.com>
2026-05-27 09:19:30 +0200

    -{ * * }- blog enum for category

---
+++ src/main/java/codewithmoise/org/blogbackend/enums/BlogCategory.java
@@ -0,0 +1,10 @@
+package codewithmoise.org.blogbackend.enums;
+
+public enum BlogCategory {
+    Entertainment,
+    Sports,
+    Transport,
+    Tech,
+    Business,
+    Education
+}
--- src/main/java/codewithmoise/org/blogbackend/models/Blog.java
+++ src/main/java/codewithmoise/org/blogbackend/models/Blog.java
@@ -1,4 +1,27 @@
 package codewithmoise.org.blogbackend.models;
 
+import codewithmoise.org.blogbackend.enums.BlogCategory;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table
+@Data
 public class Blog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "category")
+    @Enumerated(EnumType.STRING)
+    private BlogCategory category;
+
+    @Column(name = "tags")
+    private String Tags;
 }
