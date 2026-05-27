b419892d1b8d05cad9951792ca9b0ea61b9bb667
A2kmoise <abayomoise950@gmail.com>
2026-05-27 19:17:51 +0200

    -{ * * }- auth service made

--- src/main/java/codewithmoise/org/blogbackend/services/AuthService.java
+++ src/main/java/codewithmoise/org/blogbackend/services/AuthService.java
@@ -1,4 +1,21 @@
 package codewithmoise.org.blogbackend.services;
 
+import codewithmoise.org.blogbackend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
+    private final UserRepository userRepository;
+
+    public  AuthService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public void createAccount(){}
+
+    public void login(){}
+
+    public void updateProfile(){}
+
+    public void logout(){}
 }
