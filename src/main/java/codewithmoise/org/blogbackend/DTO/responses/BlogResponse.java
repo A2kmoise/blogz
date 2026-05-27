fd0ad5896ba648d4367cd0501f3a3949019b7fce
A2kmoise <abayomoise950@gmail.com>
2026-05-27 16:39:28 +0200

    -{ * * }- BlogPostResponse

---
+++ src/main/java/codewithmoise/org/blogbackend/DTO/responses/BlogResponse.java
@@ -0,0 +1,25 @@
+package codewithmoise.org.blogbackend.DTO.responses;
+
+import codewithmoise.org.blogbackend.enums.BlogCategory;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class BlogResponse {
+
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private BlogCategory category;
+
+    private List<String> tags;
+
+    private Long AuthorId;
+
+    private LocalDateTime createdAt;
+}
