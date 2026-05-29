aa5d30f10756d48298c9772824c23a259ac53882
A2kmoise <abayomoise950@gmail.com>
2026-05-29 21:48:14 +0200

    -{ * * }- Tag model update

--- src/main/java/codewithmoise/org/blogbackend/models/Tag.java
+++ src/main/java/codewithmoise/org/blogbackend/models/Tag.java
@@ -4,12 +4,13 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
-@Table
+@Table(name = "tags")
 @Data
 public class Tag {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String name;
 }
