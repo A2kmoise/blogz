46d408dbbe243ac3a5dee23fcfc41d1a38d486f6
A2kmoise <abayomoise950@gmail.com>
2026-05-30 21:26:08 +0200

    -{ * * }- Blog controller update

--- src/main/java/codewithmoise/org/blogbackend/controllers/BlogController.java
+++ src/main/java/codewithmoise/org/blogbackend/controllers/BlogController.java
@@ -3,57 +3,60 @@ package codewithmoise.org.blogbackend.controllers;
 import codewithmoise.org.blogbackend.DTO.requests.BlogPostRequest;
 import codewithmoise.org.blogbackend.DTO.requests.BlogUpdateRequest;
 import codewithmoise.org.blogbackend.DTO.responses.BlogResponse;
-import codewithmoise.org.blogbackend.models.Blog;
 import codewithmoise.org.blogbackend.services.BlogService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.support.ResourceTransactionManager;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/blog")
+@RequestMapping("/api/blogs")
+@CrossOrigin(origins = "*")
 public class BlogController {
 
     private final BlogService blogService;
-    private final ResourceTransactionManager resourceTransactionManager;
 
-    public BlogController(BlogService blogService, ResourceTransactionManager resourceTransactionManager){
+    public BlogController(BlogService blogService) {
         this.blogService = blogService;
-        this.resourceTransactionManager = resourceTransactionManager;
     }
-    @GetMapping("/")
-    public ResponseEntity<List<BlogResponse>> getBlogs(){
-        return ResponseEntity.ok().body(blogService.getBlogs());
+
+    @GetMapping
+    public ResponseEntity<List<BlogResponse>> getBlogs() {
+        return ResponseEntity.ok(blogService.getBlogs());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Optional<Blog>> getBlogById(@PathVariable Long id){
-        return ResponseEntity.ok().body(blogService.getBlogById(id));
+    public ResponseEntity<BlogResponse> getBlogById(@PathVariable Long id) {
+        Optional<BlogResponse> blog = blogService.getBlogById(id);
+        return blog.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 
-    @PostMapping("/")
-    public ResponseEntity<BlogResponse> createBlog(@Valid  @RequestBody BlogPostRequest blog){
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(blogService.createBlog(blog));
+    @GetMapping("/user/{userId}")
+    public ResponseEntity<List<BlogResponse>> getBlogsByUser(@PathVariable Long userId) {
+        return ResponseEntity.ok(blogService.getBlogsByUser(userId));
+    }
+
+    @PostMapping
+    public ResponseEntity<BlogResponse> createBlog(@Valid @RequestBody BlogPostRequest blog) {
+        BlogResponse createdBlog = blogService.createBlog(blog);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdBlog);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<BlogResponse> updateBlog(
             @Valid @RequestBody BlogUpdateRequest blogUpdateRequest,
-            @PathVariable Long id){
-        return ResponseEntity
-                .status(HttpStatus.CREATED)
-                .body(blogService.updateBlog(blogUpdateRequest, id));
+            @PathVariable Long id) {
+        BlogResponse updatedBlog = blogService.updateBlog(blogUpdateRequest, id);
+        return ResponseEntity.ok(updatedBlog);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteBlog(@PathVariable Long id){
-      blogService.deleteBlog(id);
+    public ResponseEntity<Void> deleteBlog(@PathVariable Long id) {
+        blogService.deleteBlog(id);
+        return ResponseEntity.noContent().build();
     }
-
 }
