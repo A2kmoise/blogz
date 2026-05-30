ca4ddcd5effd8db1f1235b88a6c39b23de8e81e7
A2kmoise <abayomoise950@gmail.com>
2026-05-30 21:30:16 +0200

    -{ * * }- Tag repository

--- src/main/java/codewithmoise/org/blogbackend/repository/TagRepository.java
+++ src/main/java/codewithmoise/org/blogbackend/repository/TagRepository.java
@@ -3,5 +3,8 @@ package codewithmoise.org.blogbackend.repository;
 import codewithmoise.org.blogbackend.models.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TagRepository extends JpaRepository<Tag, Long> {
+    Optional<Tag> findByName(String name);
 }
