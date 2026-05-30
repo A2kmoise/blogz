9e5fc3738677de46a081417fb1d413c7c2a06244
A2kmoise <abayomoise950@gmail.com>
2026-05-30 21:36:19 +0200

    -{ * * }- Tag CRUD service

---
+++ src/main/java/codewithmoise/org/blogbackend/services/TagService.java
@@ -0,0 +1,37 @@
+package codewithmoise.org.blogbackend.services;
+
+import codewithmoise.org.blogbackend.models.Tag;
+import codewithmoise.org.blogbackend.repository.TagRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TagService {
+    private final TagRepository tagRepository;
+
+    public TagService(TagRepository tagRepository) {
+        this.tagRepository = tagRepository;
+    }
+
+    public List<Tag> findOrCreateTags(List<String> tagNames) {
+        List<Tag> tags = new ArrayList<>();
+        
+        for (String tagName : tagNames) {
+            Tag tag = tagRepository.findByName(tagName)
+                    .orElseGet(() -> {
+                        Tag newTag = new Tag();
+                        newTag.setName(tagName);
+                        return tagRepository.save(newTag);
+                    });
+            tags.add(tag);
+        }
+        
+        return tags;
+    }
+
+    public List<Tag> getAllTags() {
+        return tagRepository.findAll();
+    }
+}
