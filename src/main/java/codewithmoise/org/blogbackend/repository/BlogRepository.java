d02e5652db315ad58dd83482583f1708e6289824
A2kmoise <abayomoise950@gmail.com>
2026-05-30 21:26:20 +0200

    -{ * * }- Blog repository update

--- src/main/java/codewithmoise/org/blogbackend/repository/BlogRepository.java
+++ src/main/java/codewithmoise/org/blogbackend/repository/BlogRepository.java
@@ -1,7 +1,15 @@
 package codewithmoise.org.blogbackend.repository;
 
+import codewithmoise.org.blogbackend.enums.BlogCategory;
 import codewithmoise.org.blogbackend.models.Blog;
+import codewithmoise.org.blogbackend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BlogRepository extends JpaRepository<Blog, Long> {
+    List<Blog> findByUser(User user);
+    List<Blog> findByCategory(BlogCategory category);
+    List<Blog> findByTitleContainingIgnoreCase(String title);
+    List<Blog> findAllByOrderByCreatedAtDesc();
 }
