dcc7bbdb343a32ebe8bfbe7d799527326374eb23
A2kmoise <abayomoise950@gmail.com>
2026-05-31 19:58:47 +0200

    -{ * * }- blog repository findAll made Desc

--- src/main/java/codewithmoise/org/blogbackend/repository/BlogRepository.java
+++ src/main/java/codewithmoise/org/blogbackend/repository/BlogRepository.java
@@ -3,6 +3,8 @@ package codewithmoise.org.blogbackend.repository;
 import codewithmoise.org.blogbackend.enums.BlogCategory;
 import codewithmoise.org.blogbackend.models.Blog;
 import codewithmoise.org.blogbackend.models.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -11,5 +13,5 @@ public interface BlogRepository extends JpaRepository<Blog, Long> {
     List<Blog> findByUser(User user);
     List<Blog> findByCategory(BlogCategory category);
     List<Blog> findByTitleContainingIgnoreCase(String title);
-    List<Blog> findAllByOrderByCreatedAtDesc();
+   Page<Blog> findAllByOrderByCreatedAtDesc(Pageable pageable);
 }
