719c573456f244a0249764a76bdd32ce7e3a7678
A2kmoise <abayomoise950@gmail.com>
2026-06-01 19:54:37 +0200

    -{ * * }- user repository

--- src/main/java/codewithmoise/org/blogbackend/repository/UserRepository.java
+++ src/main/java/codewithmoise/org/blogbackend/repository/UserRepository.java
@@ -1,11 +1,16 @@
 package codewithmoise.org.blogbackend.repository;
 
+import codewithmoise.org.blogbackend.enums.UserRoles;
 import codewithmoise.org.blogbackend.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
+    List<User> findByRole(UserRoles role);
+    long countByRole(UserRoles role);
+    long countBySuspended(boolean suspended);
 }
