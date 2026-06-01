dd6aeeaacc972be885e3cee99837b88b0a24d9b7
A2kmoise <abayomoise950@gmail.com>
2026-06-01 09:00:51 +0200

    -{ * * }- JwtUtil made

--- src/main/java/codewithmoise/org/blogbackend/security/JwtAuthenticationFilter.java
+++ src/main/java/codewithmoise/org/blogbackend/security/JwtAuthenticationFilter.java
@@ -19,8 +19,14 @@ import java.util.Collections;
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    @Autowired
-    private JwtUtil jwtUtil;
+    //@Autowired
+    //private JwtUtil jwtUtil; Field injection
+
+    private final JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil){
+        this.jwtUtil = jwtUtil;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
