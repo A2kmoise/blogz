38c366733a4c79f139bb8a51d83aed70ed742fa8
A2kmoise <abayomoise950@gmail.com>
2026-06-01 09:19:46 +0200

    -{ * * }- separate logics

--- src/main/java/codewithmoise/org/blogbackend/security/SecurityConfig.java
+++ src/main/java/codewithmoise/org/blogbackend/security/SecurityConfig.java
@@ -2,8 +2,10 @@ package codewithmoise.org.blogbackend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,12 +17,21 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
+              /*  .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))*/
+
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/blogs/**").hasRole("AUTHOR")
-                        .requestMatchers("/api/admin/").hasRole("ADMIN")
-                        .requestMatchers("/auth/").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/api/blogs/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/blogs/**").hasRole("AUTHOR")
+                        .requestMatchers(HttpMethod.DELETE, "/api/blogs/**").hasRole("AUTHOR")
+                        .requestMatchers(HttpMethod.PUT,"/api/blogs/**").hasRole("AUTHOR")
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
                         .anyRequest().authenticated()
+
+
                 );
 
         return http.build();
