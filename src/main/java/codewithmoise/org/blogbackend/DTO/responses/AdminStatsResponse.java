a32e9d3a8efce3c30552e41383c3f6ea8758f618
A2kmoise <abayomoise950@gmail.com>
2026-06-01 19:58:53 +0200

    -{ * * }- admin stats dto response

--- src/main/java/codewithmoise/org/blogbackend/DTO/responses/AdminStatsResponse.java
+++ src/main/java/codewithmoise/org/blogbackend/DTO/responses/AdminStatsResponse.java
@@ -4,15 +4,13 @@ import lombok.Data;
 
 @Data
 public class AdminStatsResponse {
-    private long totalUsers;
     private long totalAuthors;
     private long totalBlogs;
     private long suspendedUsers;
 
-    public  AdminStatsResponse(long totalAuthors, long totalBlogs, long totalUsers, long suspendedUsers){
+    public  AdminStatsResponse(long totalAuthors, long totalBlogs,long suspendedUsers){
         this.suspendedUsers = suspendedUsers;
         this.totalAuthors = totalAuthors;
         this.totalBlogs = totalBlogs;
-        this.totalUsers = totalUsers;
     }
 }
