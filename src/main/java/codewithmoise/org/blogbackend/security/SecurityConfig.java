e7c4eb2c843802dd8b13194cf8eb0bea5beac39e
A2kmoise <abayomoise950@gmail.com>
2026-06-01 09:20:47 +0200

    -{ * * }- CSRF disabled

--- src/main/java/codewithmoise/org/blogbackend/security/SecurityConfig.java
+++ src/main/java/codewithmoise/org/blogbackend/security/SecurityConfig.java
@@ -17,9 +17,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-              /*  .csrf(csrf -> csrf.disable())
+                .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))*/
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.GET,"/api/blogs/**").permitAll()
