f0373e1b94c4c1d45df7329cbada59a7f21c972e
A2kmoise <abayomoise950@gmail.com>
2026-06-04 11:25:05 +0200

    -{ * * }- added path to not block HttpMethod PATCH

--- src/main/java/codewithmoise/org/blogbackend/config/WebConfig.java
+++ src/main/java/codewithmoise/org/blogbackend/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOriginPatterns(List.of("http://localhost:3000"));
-        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
         config.setMaxAge(3600L);
