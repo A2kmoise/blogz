04ad5ac592fa1cc6f8d28b6decdeb66fcc142b8a
A2kmoise <abayomoise950@gmail.com>
2026-06-04 11:23:25 +0200

    -{ * * }- Tag repository jpql to delete leftup tags

--- src/main/java/codewithmoise/org/blogbackend/repository/TagRepository.java
+++ src/main/java/codewithmoise/org/blogbackend/repository/TagRepository.java
@@ -2,9 +2,16 @@ package codewithmoise.org.blogbackend.repository;
 
 import codewithmoise.org.blogbackend.models.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface TagRepository extends JpaRepository<Tag, Long> {
     Optional<Tag> findByName(String name);
+
+    @Modifying
+    @Query("DELETE FROM Tag t WHERE SIZE(t.blogs) = 0 ")
+    void deleteOrphanedTags();
+
 }
