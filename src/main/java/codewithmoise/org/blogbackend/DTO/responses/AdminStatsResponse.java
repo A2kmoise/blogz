81f8595e9f302cd1282a8679fcab9f79c0389c33
A2kmoise <abayomoise950@gmail.com>
2026-06-04 11:17:12 +0200

    -{ * * }- admin stats response

--- src/main/java/codewithmoise/org/blogbackend/DTO/responses/AdminStatsResponse.java
+++ src/main/java/codewithmoise/org/blogbackend/DTO/responses/AdminStatsResponse.java
@@ -1,16 +1,32 @@
 package codewithmoise.org.blogbackend.DTO.responses;
 
 import lombok.Data;
+import java.util.List;
 
 @Data
 public class AdminStatsResponse {
-    private long totalAuthors;
     private long totalBlogs;
-    private long suspendedUsers;
+    private long totalAuthors;
+    private long totalAdmins;
+    private List<ActivityResponse> recentActivity;
 
-    public  AdminStatsResponse(long totalAuthors, long totalBlogs,long suspendedUsers){
-        this.suspendedUsers = suspendedUsers;
-        this.totalAuthors = totalAuthors;
+    public AdminStatsResponse(long totalBlogs, long totalAuthors, long totalAdmins, List<ActivityResponse> recentActivity) {
         this.totalBlogs = totalBlogs;
+        this.totalAuthors = totalAuthors;
+        this.totalAdmins = totalAdmins;
+        this.recentActivity = recentActivity;
+    }
+
+    @Data
+    public static class ActivityResponse {
+        private String id;
+        private String text;
+        private String date;
+
+        public ActivityResponse(String id, String text, String date) {
+            this.id = id;
+            this.text = text;
+            this.date = date;
+        }
     }
 }
